fe43612415a13d52500eb69ace58c2d7e6a8ee87
Samuel Lyons <samuellyons@Samuels-MacBook-Air.local>
2022-03-23 09:03:15 -0400

    Updated project to allow players to input their names. Also added more javascript.

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const gameCells = document.querySelectorAll(".cell");
 const newGameButton = document.getElementById('newGame');
+let playerOne = document.getElementById('player1');
+let playerTwo = document.getElementById('player2');
+let player1 = document.getElementById('nameOne');
+let subButton = document.getElementById('submit');
 
+
+function names() {
+    playerOne.textContent = "Player one is" + " " + prompt("Please enter player one's name") + " " + "(Blue)" + " " + "click your squares once.";
+    playerTwo.textContent = "Player two is" + " " + prompt("Please enter player two's name") + " " + "(Red)" + " " + "click your squares twice.";
+
+
+}
+
+names()
 
 function startGame(){
 
@@ -26,5 +39,8 @@ gameCells.forEach((gameCells) =>{
 
 })
 
+
+
 startGame();
+
 
